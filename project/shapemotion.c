04e344ba0b2e907b33b3c24dbe348b070e98982f
Emmanuel Guevara <eguevara4@miners.utep.edu>
2016-11-30 00:08:04 -0700

    Finished game without sound

--- project/shapemotion.c
+++ project/shapemotion.c
@@ -19,50 +19,54 @@
 
 #define GREEN_LED BIT6
 
+u_int score = 0;
+u_int level = 1;
+u_int bombs = 2;
+u_char gameOn = 1;
 
-AbRect rect10 = {abRectGetBounds, abRectCheck, {0,3}}; /**< 10x10 rectangle */
+AbRect rect10 = {abRectGetBounds, abRectCheck, {0,1}}; /**< 10x10 rectangle */
 AbShip  ship = {abShipGetBounds, abShipCheck, 20};
 
 AbRectOutline fieldOutline = {	/* playing field */
   abRectOutlineGetBounds, abRectOutlineCheck,   
-  {screenWidth/2 - 10, screenHeight/2 - 5}
+  {screenWidth/2 - 1, screenHeight/2 - 10}
 };
 
 Layer laser = {		/**< Layer with an orange circle */
   (AbShape *)&rect10,
   {(screenWidth/2)-10, screenHeight-15}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_RED,
+  COLOR_GREEN,
   0,
 };
 
 Layer layer3 = {		/**< Layer with an orange circle */
   (AbShape *)&circle6,
-  {(screenWidth/2)-10, (screenHeight/2)+5}, /**< bit below & right of center */
+  {(screenWidth/2)-30, 30}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_VIOLET,
+  COLOR_RED,
   &laser,
 };
 
 Layer layer2 = {		/**< Layer with an orange circle */
   (AbShape *)&circle6,
-  {(screenWidth/2)+20, (screenHeight/2)-25}, /**< bit below & right of center */
+  {(screenWidth/2), 30}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_VIOLET,
+  COLOR_RED,
   &layer3,
 };
 
 Layer layer1 = {		/**< Layer with an orange circle */
   (AbShape *)&circle6,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
+  {(screenWidth/2)+30, 30}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_VIOLET,
+  COLOR_RED,
   &layer2,
 };
 
 Layer fieldLayer = {		/* playing field as a layer */
   (AbShape *) &fieldOutline,
-  {screenWidth/2-9, screenHeight/2+4},/**< center */
+  {screenWidth/2, screenHeight/2+9},/**< center */
   {0,0}, {0,0},				    /* last & next pos */
   COLOR_GRAY,
   &layer1
@@ -95,8 +99,6 @@ MovLayer ml0 = { &shipLayer, {0,0}, &ml2 };
 
 
 
-
-
 movLayerDraw(MovLayer *movLayers, Layer *layers)
 {
   int row, col;
@@ -108,6 +110,40 @@ movLayerDraw(MovLayer *movLayers, Layer *layers)
     l->posLast = l->pos;
     l->pos = l->posNext;
   }
+  
+  u_int temp = score;              /* Draws the score */
+  u_int offset = 120;
+  drawString5x7(60,1,"Score:",COLOR_GRAY,COLOR_BLACK);
+  drawString5x7(1,1,"Level:",COLOR_GRAY,COLOR_BLACK);
+  drawString5x7(1,10,"Bombs:   ",COLOR_GRAY,COLOR_BLACK);
+  
+  while(temp){
+    drawDigit5x7(offset,1,temp%10,COLOR_GRAY,COLOR_BLACK);
+    temp /= 10;
+    offset -= 6;
+  }
+  
+  temp = level;              /* Draws the level */
+  offset = 50;
+  while(temp){
+    drawDigit5x7(offset,1,temp%10,COLOR_GRAY,COLOR_BLACK);
+    temp /= 10;
+    offset -= 6;
+  }
+  
+  temp = bombs;              /* Draws the bombs left */
+  offset = 50;
+  if(bombs != 0){
+  while(temp){
+    drawDigit5x7(offset,10,temp%10,COLOR_GRAY,COLOR_BLACK);
+    temp /= 10;
+    offset -= 6;
+  }
+  }else{
+    drawString5x7(1,10,"Bombs:   ",COLOR_GRAY,COLOR_BLACK);
+    drawString5x7(1,10,"Bombs:  0",COLOR_GRAY,COLOR_BLACK);
+  }
+  
   or_sr(8);			/**< disable interrupts (GIE on) */
 
 
@@ -128,7 +164,7 @@ movLayerDraw(MovLayer *movLayers, Layer *layers)
 	    break; 
 	  } /* if probe check */
 	} // for checking all layers at col, row
-	lcd_writeColor(color); 
+	lcd_writeColor(color);
       } // for col
     } // for row
   } // for moving layer being updated.
@@ -160,33 +196,124 @@ void mlAdvance(MovLayer *ml, Region *fence)
       }	/**< if outside of fence */
     } /**< for axis */
     ml->layer->posNext = newPos;
-    if(ml->velocity.axes[1] < -5 && ml->layer->pos.axes[1] < 30){
+    if(ml->velocity.axes[1] < -5 && ml->layer->pos.axes[1] < 50){
       ml->layer->color = COLOR_BLACK;
     }
     if(ml->velocity.axes[1] > 3){
-      ml->velocity.axes[1] = 0;
+      ml5.velocity.axes[1] = 0;
+      ml5.layer->pos.axes[0] = ml0.layer->pos.axes[0];
+      ml5.layer->pos.axes[1] = ml0.layer->pos.axes[1]-10;
+      ml5.layer->posLast.axes[0] = ml0.layer->posLast.axes[0];
+      ml5.layer->posLast.axes[1] = ml0.layer->posLast.axes[1];
+      ml5.layer->posNext.axes[0] = ml0.layer->posNext.axes[0];
+      ml5.layer->posNext.axes[1] = ml0.layer->posNext.axes[1];
     }
   } /**< for ml */
+}
+
+void adjustLevel(MovLayer *ml){
+  switch(level){
+  case 1:
+    if(ml->velocity.axes[0] > 0)
+      ml->velocity.axes[0] = 1;
+    else
+      ml->velocity.axes[0] = -1;
+    if(ml->velocity.axes[1] > 0)
+      ml->velocity.axes[1] = 1;
+    else
+      ml->velocity.axes[1] = -1;
+    break;
+  case 2:
+    if(ml->velocity.axes[0] > 0)
+      ml->velocity.axes[0] = 1;
+    else
+      ml->velocity.axes[0] = -1;
+    if(ml->velocity.axes[1] > 0)
+      ml->velocity.axes[1] = 2;
+    else
+      ml->velocity.axes[1] = -2;
+    break;
+  case 3:
+    if(ml->velocity.axes[0] > 0)
+      ml->velocity.axes[0] = 2;
+    else
+      ml->velocity.axes[0] = -2;
+    if(ml->velocity.axes[1] > 0)
+      ml->velocity.axes[1] = 2;
+    else
+      ml->velocity.axes[1] = -2;
+    break;
+  case 4:
+    if(ml->velocity.axes[0] > 0)
+      ml->velocity.axes[0] = 2;
+    else
+      ml->velocity.axes[0] = -2;
+    if(ml->velocity.axes[1] > 0)
+      ml->velocity.axes[1] = 3;
+    else
+      ml->velocity.axes[1] = -3;
+    break;
+  default:
+    if(ml->velocity.axes[0] > 0)
+      ml->velocity.axes[0] = 3;
+    else
+      ml->velocity.axes[0] = -3;
+    if(ml->velocity.axes[1] > 0)
+      ml->velocity.axes[1] = 3;
+    else
+      ml->velocity.axes[1] = -3;
+    break;
+  }
 }
 
 void checkCollision(MovLayer *ml){
   u_char axis;
   u_char step = 0;
-
+  
   ml = ml->next;
+  
   for (; ml && step < 3; ml = ml->next) {
-    //abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+
+    adjustLevel(ml);
+    if(ml0.layer->pos.axes[0] < ml->layer->pos.axes[0] + 10 &&
+        ml0.layer->pos.axes[0] > ml->layer->pos.axes[0] - 10 &&
+        ml0.layer->pos.axes[1] < ml->layer->pos.axes[1] + 10 &&
+        ml0.layer->pos.axes[1] > ml->layer->pos.axes[1] - 10){
+
+      gameOn = 0;
+      return;
+    }
+      
       if(ml5.layer->pos.axes[0] < ml->layer->pos.axes[0] + 7 &&
         ml5.layer->pos.axes[0] > ml->layer->pos.axes[0] - 7 &&
-        ml5.layer->pos.axes[1] < ml->layer->pos.axes[1] + 7 &&
-        ml5.layer->pos.axes[1] > ml->layer->pos.axes[1] - 7){
-      if(ml5.velocity.axes[1] < -3){
-        ml5.velocity.axes[1] = 0;
-        ml5.layer->color = COLOR_BLACK;
-      }
-      ml->layer->posNext.axes[1] = 20;
-      return;
-    } /**< for axis */
+        ml5.layer->pos.axes[1] < ml->layer->pos.axes[1] - 4 &&
+        ml5.layer->pos.axes[1] > ml->layer->pos.axes[1] - 15){
+
+	score++;
+	if(level < 5){
+          level = (score/10)+1;
+	}else{
+	  level = 5;
+	}
+	if(score % 20 == 0){
+	  bombs++;
+	}
+	if(ml5.velocity.axes[1] < -5 && ml5.layer->pos.axes[1] < 30){
+          ml5.layer->color = COLOR_BLACK;
+        }
+	if(ml5.velocity.axes[1] < -3){
+          ml5.velocity.axes[1] = 0;
+          ml5.layer->pos.axes[0] = ml0.layer->pos.axes[0];
+	  ml5.layer->pos.axes[1] = ml0.layer->pos.axes[1]-10;
+	  ml5.layer->posLast.axes[0] = ml0.layer->posLast.axes[0];
+          ml5.layer->posLast.axes[1] = ml0.layer->posLast.axes[1];
+	  ml5.layer->posNext.axes[0] = ml0.layer->posNext.axes[0];
+	  ml5.layer->posNext.axes[1] = ml0.layer->posNext.axes[1];
+          ml5.layer->color = COLOR_BLACK;
+        }
+        ml->layer->posNext.axes[1] = 30;
+        return;
+      }/**< for axis */
     step++;
   }
 }
@@ -199,6 +326,7 @@ Region fieldFence;		/**< fence around playing field  */
 /** Initializes everything, enables interrupts and green LED, 
  *  and handles the rendering for the screen
  */
+
 void main()
 {
   P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
@@ -213,11 +341,12 @@ void main()
 
   layerGetBounds(&fieldLayer, &fieldFence);
 
-
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
   
-  drawString5x7(1,1,"Score:",COLOR_GRAY,COLOR_BLACK);
+  drawString5x7(60,1,"Score:    0",COLOR_GRAY,COLOR_BLACK);
+  drawString5x7(1,1,"Level:  1",COLOR_GRAY,COLOR_BLACK);
+  drawString5x7(1,10,"Bombs:  1",COLOR_GRAY,COLOR_BLACK);
   for(;;) { 
     while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
       P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
@@ -225,10 +354,16 @@ void main()
     }
     P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
     redrawScreen = 0;
-    movLayerDraw(&ml0, &shipLayer);
+    
+    if(!gameOn){
+      drawString5x7(40,80,"YOU LOSE",COLOR_GRAY,COLOR_BLACK);
+    }else{
+      movLayerDraw(&ml0, &shipLayer);
+    }
   }
 }
 
+char down = 0;
 /** Watchdog timer interrupt handler. 15 interrupts/sec */
 void wdt_c_handler()
 {
@@ -237,6 +372,10 @@ void wdt_c_handler()
   count ++;
   if (count == 15) {
     u_int switches = p2sw_read();
+    if(!gameOn){
+      return;
+    }
+    checkCollision(&ml0);
     mlAdvance(&ml0, &fieldFence);
     if (~switches & 1){
       ml0.velocity.axes[0] = -3;
@@ -253,24 +392,35 @@ void wdt_c_handler()
       ml5.velocity.axes[0] = 0;
     }
     if(~switches & 4){
-      
+      down = 1;
+    }
+    if(switches & 4 && down){
+      if(bombs > 0){
+	bombs--;
+	ml2.layer->posNext.axes[1] = 30;
+	ml3.layer->posNext.axes[1] = 30;
+	ml4.layer->posNext.axes[1] = 30;
+	score += 3;
+	level = (score/10)+1;
+      }
+      down = 0;
     }
     if(~switches & 8){
       if(ml5.velocity.axes[1] == 0){
-	ml5.velocity.axes[1] = -6;
+	ml5.velocity.axes[1] = -8;
 	ml5.velocity.axes[0] = 0;
 	ml5.layer->pos.axes[0] = ml0.layer->pos.axes[0];
-	ml5.layer->pos.axes[1] = ml0.layer->pos.axes[1];
+	ml5.layer->pos.axes[1] = ml0.layer->pos.axes[1]-10;
 	ml5.layer->posLast.axes[0] = ml0.layer->posLast.axes[0];
 	ml5.layer->posLast.axes[1] = ml0.layer->posLast.axes[1];
 	ml5.layer->posNext.axes[0] = ml0.layer->posNext.axes[0];
 	ml5.layer->posNext.axes[1] = ml0.layer->posNext.axes[1];
-	ml5.layer->color = COLOR_RED;
+	ml5.layer->color = COLOR_GREEN;
       }
     }
     redrawScreen = 1;
     count = 0;
   }
-  checkCollision(&ml0);
+  
   P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
 }
